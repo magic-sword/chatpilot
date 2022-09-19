e657effc34b471e47732fb9036878233b4a4e27b
maken0120 <magic.sword.0001@gmail.com>
2022-09-19 11:54:33 +0900

    ポータブルアプリとしてビルドできるように修正

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  // ポータブルアプリとして出力するように設定
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: 'src/assets/chatpilot-logo.png',
+          target: [
+            {
+              target: 'portable', // 'zip', 'nsis', 'portable'
+              arch: ['x64'], // 'x64', 'ia32'
+            },
+          ],
+        },
+      }
+    }
+  }
 });
